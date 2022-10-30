e387f7cb01a50b408aaba45f1f19bd165101f06b
Micke Berg <micke.berg@gmail.com>
2022-10-30 16:12:35 +0100

    new categories component

---
+++ src/components/GameCategories/GameCategories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Tags } from '../../types';
+import Button from '../Button/Button';
+
+type Props = {
+	tagsInFilteredGames: Tags[];
+	handleFilterCategories: (id: number) => void;
+	handleResetToAllGames: () => void;
+};
+
+const GameCategories = ({
+	tagsInFilteredGames,
+	handleFilterCategories,
+	handleResetToAllGames,
+}: Props) => {
+	return (
+		<div>
+			<h2 className="header">Categories</h2>
+			<div className="grid">
+				{tagsInFilteredGames?.map((tag) => (
+					<Button key={tag.id} onClick={() => handleFilterCategories(tag.id)}>
+						{tag.name}
+					</Button>
+				))}
+				<Button
+					title="All games"
+					style={{
+						backgroundColor: 'orange',
+					}}
+					onClick={() => handleResetToAllGames()}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default GameCategories;
